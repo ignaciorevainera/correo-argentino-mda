69ea4f1f2eff569180c2159051a10fc12cfdddf1
Franco <gonzalezf503@gmail.com>
2026-06-27 19:14:21 -0300

    update: Modularization in BaseLayout.astro - components & modals

---
+++ src/components/ui/modals/index.ts
@@ -0,0 +1,3 @@
+export { default as AboutProjectModal } from './aboutProjectModal.astro';
+export { default as CommandPaletteModal } from './commandPaletteModal.astro'
+export { default as FeedbackModal } from './feedbackModal.astro'
