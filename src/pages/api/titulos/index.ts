32ba6a5da7e835057a279938b276c15613be1dc9
Franco <gonzalezf503@gmail.com>
2026-07-01 16:57:28 -0300

    chore: created titulos api

---
+++ src/pages/api/titulos/index.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+import { db } from "@/db"
+import { titles, titleCategory } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+
+export const GET: APIRoute = async () => {
+    const data = await db.select({
+        id: titles.id,
+        name: titles.name,
+        category: titleCategory.name,
+        icon: titleCategory.icon,
+        tone: titleCategory.tone,
+        route: titles.route,
+        description: titles.description,
+        articleOnKdb: titles.articleOnKdb,
+    }).from(titles)
+        .leftJoin(
+            titleCategory,
+            eq(titles.categoryId, titleCategory.id)
+        );
+
+    return Response.json(data);
+}
